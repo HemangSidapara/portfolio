57d71931d908237702db02645b000965b93b7684
Hemang Sidapara <61095579+HemangSidapara@users.noreply.github.com>
2022-01-03 17:09:46 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,328 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>MindWave Portfolio</title>
+
+    <!-- Custom fonts for this template -->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Custom styles for this template -->
+    <link href="css/creative.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+      <div class="container">
+        <a class="navbar-brand" href="#">Mind Wave</a>
+        <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarResponsive" >
+          <span class="navbar-toggler-icon"></span>
+        </button>  
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="#about">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#services">Services</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#portfolio">Portfolio</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#ourteam">Our Team</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <header class="masthead text-center text-white d-flex">
+      <div class="container my-auto">
+        <div class="row">
+          <div class="col-lg-10 mx-auto">
+            <h1 class="text-uppercase">
+              <strong>Your sky limites software development</strong>
+            </h1>
+            <hr>
+          </div>
+          <div class="col-lg-8 mx-auto">
+            <p class="text-faded mb-5">Never outsourced. We are a software consulting firm that specializes in iOS & Android mobile app and Web design and development!</p>
+            <a class="btn btn-primary btn-xl js-scroll-trigger" href="#about">Find Out More</a>
+          </div>
+        </div>
+      </div>
+    </header>
+
+    <section class="bg-primary" id="about">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 mx-auto text-center">
+            <h2 class="section-heading text-white">We've got what you need!</h2>
+            <hr class="light my-4">
+            <p class="text-faded mb-4">Mind Wave has everything you need to get your new business up and running in no time! If you are considering now that I am starting my new work on digital ! Do not think more about this, we are providing the sky for the fly.</p>
+            <a class="btn btn-light btn-xl js-scroll-trigger" href="#services">Get Started!</a>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section id="services">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12 text-center">
+            <h2 class="section-heading">Our Services</h2>
+            <hr class="my-4">
+          </div>
+        </div>
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-3 col-md-6 text-center">
+            <div class="service-box mt-5 mx-auto">
+              <i class="fas fa-drafting-compass text-primary mb-3 sr-icon-1"></i>
+              <h3 class="mb-3">Graphics Designing</h3>
+              <p class="text-muted mb-0">We begin every Mobile or Web project by crafting a specific strategy based on the project requirements, including your aesthetic desires, brand needs, business needs.</p>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-6 text-center">
+            <div class="service-box mt-5 mx-auto">
+              <i class="fab fa-app-store-ios text-primary mb-3 sr-icon-2"></i>
+              <h3 class="mb-3">iOS App Development</h3>
+              <p class="text-muted mb-0">Mind Wave is one of the emerging leaders and specialist in iPhone application development as well as iPad Application development.</p>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-6 text-center">
+            <div class="service-box mt-5 mx-auto">
+              <i class="fab fa-android text-primary mb-3 sr-icon-3"></i>
+              <h3 class="mb-3">Andriod App Development</h3>
+              <p class="text-muted mb-0">We provide full, end-to-end mobile application development service, including design, coding, testing, deployment, promotion and support.</p>
+            </div>
+          </div>
+          
+          <div class="col-lg-3 col-md-6 text-center">
+            <div class="service-box mt-5 mx-auto">
+              <i class="fab fa-safari text-primary mb-3 sr-icon-4"></i>
+              <h3 class="mb-3">Web Development</h3>
+              <p class="text-muted mb-0">You have to make your websites with love these days! If "Mobile First", it is impossible to imagine mobile without web. We build robust mobile backend as well as do develop web applications. </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="p-0" id="portfolio">
+      <div class="container-fluid p-0">
+        <div class="row no-gutters">
+          <div class="col-lg-4 col-sm-6">
+            <a class="portfolio-box" href="img/portfolio/fullsize/1.jpg">
+              <img class="img-fluid" src="img/portfolio/thumbnails/1.jpg" alt="">
+              <div class="portfolio-box-caption">
+                <div class="portfolio-box-caption-content">
+                  <div class="project-category text-faded">
+                    Web Design
+                  </div>
+                  <div class="project-name">
+                    Project 1
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="col-lg-4 col-sm-6">
+            <a class="portfolio-box" href="img/portfolio/fullsize/2.jpg">
+              <img class="img-fluid" src="img/portfolio/thumbnails/2.jpg" alt="">
+              <div class="portfolio-box-caption">
+                <div class="portfolio-box-caption-content">
+                  <div class="project-category text-faded">
+                    Android App
+                  </div>
+                  <div class="project-name">
+                    Project 2
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="col-lg-4 col-sm-6">
+            <a class="portfolio-box" href="img/portfolio/fullsize/3.jpg">
+              <img class="img-fluid" src="img/portfolio/thumbnails/3.jpg" alt="">
+              <div class="portfolio-box-caption">
+                <div class="portfolio-box-caption-content">
+                  <div class="project-category text-faded">
+                    iOS App
+                  </div>
+                  <div class="project-name">
+                    Project 3
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="col-lg-4 col-sm-6">
+            <a class="portfolio-box" href="img/portfolio/fullsize/4.jpg">
+              <img class="img-fluid" src="img/portfolio/thumbnails/4.jpg" alt="">
+              <div class="portfolio-box-caption">
+                <div class="portfolio-box-caption-content">
+                  <div class="project-category text-faded">
+                    UI/UX Graphics
+                  </div>
+                  <div class="project-name">
+                    Project 4
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="col-lg-4 col-sm-6">
+            <a class="portfolio-box" href="img/portfolio/fullsize/5.jpg">
+              <img class="img-fluid" src="img/portfolio/thumbnails/5.jpg" alt="">
+              <div class="portfolio-box-caption">
+                <div class="portfolio-box-caption-content">
+                  <div class="project-category text-faded">
+                    Marketing
+                  </div>
+                  <div class="project-name">
+                    Project 5
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="col-lg-4 col-sm-6">
+            <a class="portfolio-box" href="img/portfolio/fullsize/6.jpg">
+              <img class="img-fluid" src="img/portfolio/thumbnails/6.jpg" alt="">
+              <div class="portfolio-box-caption">
+                <div class="portfolio-box-caption-content">
+                  <div class="project-category text-faded">
+                    Unity 
+                  </div>
+                  <div class="project-name">
+                    Project 6
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section id="ourteam">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12 text-center">
+            <h2 class="section-heading">Our Team</h2>
+            <hr class="my-3">
+          </div>
+        </div>
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-4 col-md-8 text-center">
+            <div class="service-box mt-5 mx-auto">
+              <i class="fas fa-user text-primary mb-3 sr-icon-1"></i>
+              <h3 class="mb-3">Team Member 1</h3>
+              <p class="text-muted mb-0">Member 1 Details.</p>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-8 text-center">
+            <div class="service-box mt-5 mx-auto">
+              <i class="fas fa-user-circle text-primary mb-3 sr-icon-2"></i>
+              <h3 class="mb-3">Team Member 2</h3>
+              <p class="text-muted mb-0">Member 2 Details.</p>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-8 text-center">
+            <div class="service-box mt-5 mx-auto">
+              <i class="fas fa-user-clock text-primary mb-3 sr-icon-3"></i>
+              <h3 class="mb-3">Team Member 3</h3>
+              <p class="text-muted mb-0">Member 3 Details.</p>
+            </div>
+          </div>
+          
+          <div class="col-lg-4 col-md-8 text-center">
+            <div class="service-box mt-5 mx-auto">
+              <i class="fas fa-user-astronaut text-primary mb-3 sr-icon-4"></i>
+              <h3 class="mb-3">Team Member 4</h3>
+              <p class="text-muted mb-0">Member 4 Details.</p>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-8 text-center">
+            <div class="service-box mt-5 mx-auto">
+              <i class="fas fa-user-check text-primary mb-3 sr-icon-4"></i>
+              <h3 class="mb-3">Team Member 5</h3>
+              <p class="text-muted mb-0">Member 5 Details.</p>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-8 text-center">
+            <div class="service-box mt-5 mx-auto">
+              <i class="fas fa-user-graduate text-primary mb-3 sr-icon-4"></i>
+              <h3 class="mb-3">Team Member 6</h3>
+              <p class="text-muted mb-0">Member 5 Details.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </sectio>
+
+    <section id="contact">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 mx-auto text-center">
+            <h2 class="section-heading">Let's Get In Touch!</h2>
+            <hr class="my-4">
+            <p class="mb-5">Ready to start your next project with us? That's great! Give us a call or send us an email and we will get back to you as soon as possible!</p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-lg-4 ml-auto text-center">
+            <i class="fas fa-phone fa-3x mb-3 sr-contact-1"></i>
+            <p>123-456-6789</p>
+          </div>
+          <div class="col-lg-4 mr-auto text-center">
+            <i class="fas fa-envelope fa-3x mb-3 sr-contact-2"></i>
+            <p>
+              <a href="mailto:your-email@your-domain.com">mindwave.pvt.lmt@gmail.com</a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="vendor/scrollreveal/scrollreveal.min.js"></script>
+    <script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="js/creative.min.js"></script>
+
+  </body>
+
+</html>
